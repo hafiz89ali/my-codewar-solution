15f92da7205eb829c7d538c94e8bb8a410b5caa3
Hafizuddin Ali <hafiz89.ali@gmail.com>
2024-10-09 17:34:44 +0800

    Organize files

---
+++ index.js
@@ -0,0 +1,7 @@
+import spinWords from "./kata/Stop-gninnipS-My-sdroW!.js";
+
+const input = "Hey fellow warriors";
+
+const output = spinWords(input);
+
+console.log(output);
--- kata/Stop-gninnipS-My-sdroW!.js
+++ kata/Stop-gninnipS-My-sdroW!.js
@@ -1,3 +1,7 @@
+// Name: Stop gninnipS My sdroW!
+// ID: 5264d2b162488dc400000001
+// URL: https://www.codewars.com/kata/5264d2b162488dc400000001
+
 // Description:
 // Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
 
@@ -31,4 +35,4 @@ function spinWords(string) {
   return result.join(" ");
 }
 
-console.log(spinWords("Hey fellow warriors!"));
+export default spinWords;
